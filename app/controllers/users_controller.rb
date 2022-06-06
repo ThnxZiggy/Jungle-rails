cd31d88822c4eaef9b104af7316508d7404447ce
Manmohit Matharu <manmohit.matharu@gmail.com>
2022-06-06 17:51:43 -0400

    user authentication complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,12 @@
 class UsersController < ApplicationController
 
   def new
-
   end
 
   def create
     user = User.new(user_params)
     if user.save
-      session[:user_id] = user_id
+      session[:user] = user_id
       redirect_to '/'
     else
       redirect_to '/signup'
@@ -17,4 +16,5 @@ class UsersController < ApplicationController
   private
     def user_params
       params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,9 +19,13 @@ Rails.application.routes.draw do
 
   get 'about/index'
 
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  get '/logout' => 'sessions#destroy'
   
   get '/signup' => 'users#new'
   post '/users' => 'users#create'
+
   
   
   # The priority is based upon order of creation: first created -> highest priority.
