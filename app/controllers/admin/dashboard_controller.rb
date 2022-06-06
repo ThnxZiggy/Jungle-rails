113ded6905d266e42947ead70fd98e9c674d611a
Manmohit Matharu <manmohit.matharu@gmail.com>
2022-06-06 14:45:08 -0400

    .

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,14 @@
 class Admin::DashboardController < ApplicationController
   def show
+    @categories = Category.count(:id)
+    @products = Product.all.count
+  end
+
+  def category_count
+    
+  end
+
+  def product_count
+    
   end
 end
